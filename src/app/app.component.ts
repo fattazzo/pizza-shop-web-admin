6d7ba1ec05c81ec2bebaa6e3ebcc23a94cc80783
fattazzo82 <fattazzo82@gmail.com>
2020-03-26 20:54:57 +0100

    themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomHandler } from './dom-handler';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pizzaroad-web-app';
+
+  theme = 'rhea';
+
+  changeTheme(event, theme: string, dark: boolean) {
+    let themeElement = document.getElementById('theme-css');
+    themeElement.setAttribute('href', themeElement.getAttribute('href').replace(this.theme, theme));
+    let themeColorElement = document.getElementById('theme-color-css');
+    themeColorElement.setAttribute('href', themeColorElement.getAttribute('href').replace(this.theme, theme));
+    this.theme = theme;
+    const hasBodyDarkTheme = DomHandler.hasClass(document.body, 'dark-theme');
+
+    if (dark) {
+      if (!hasBodyDarkTheme) {
+        DomHandler.addClass(document.body, 'dark-theme');
+      }
+    }
+    else if (hasBodyDarkTheme) {
+      DomHandler.removeClass(document.body, 'dark-theme');
+    }
+
+    event.preventDefault();
+
+  }
 }
